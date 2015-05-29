00fdf091e2f30eb1638e36df0a0c5ef0cb33eb57
mikha nobot <mikasaur@gmail.com>
2015-05-29 18:59:35 +0200

    #1 Added Basic Player Movement
    progress!

---
+++ Assets/turn.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKey ("q")) {
+		transform.Rotate(Vector3.down * 3,Space.Self);
+	}
+		if (Input.GetKey ("e")) {
+		transform.Rotate(Vector3.up * 3,Space.Self);
+	}
+		if (Input.GetKey ("w")) {
+		transform.Translate(Vector3.forward / 7);
+	}
+		if (Input.GetKey ("s")) {
+		transform.Translate(Vector3.back / 7);
+	}
+		if (Input.GetKey ("a")) {
+		transform.Translate(Vector3.left / 7);
+	}
+		if (Input.GetKey ("d")) {
+		transform.Translate(Vector3.right / 7);
+	}
+}
