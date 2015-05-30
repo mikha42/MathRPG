05db78afe67a529eff648aa388221895fdfcfd34
mikha nobot <mikasaur@gmail.com>
2015-05-30 13:03:30 +0200

    #1 Player Modifications And Bug Fix
    Camera fixed and the only thing left is dodging

--- Assets/Player/turn.js
+++ Assets/Player/turn.js
@@ -6,21 +6,21 @@ function Start () {
 
 function Update () {
 	if (Input.GetKey ("q")) {
-		transform.Rotate(Vector3.down * 3,Space.Self);
+		transform.Rotate(Vector3.down * 2,Space.Self);
 	}
 		if (Input.GetKey ("e")) {
-		transform.Rotate(Vector3.up * 3,Space.Self);
+		transform.Rotate(Vector3.up * 2,Space.Self);
 	}
 		if (Input.GetKey ("w")) {
-		transform.Translate(Vector3.forward / 7);
+		transform.Translate(Vector3.forward / 15);
 	}
 		if (Input.GetKey ("s")) {
-		transform.Translate(Vector3.back / 7);
+		transform.Translate(Vector3.back / 15);
 	}
 		if (Input.GetKey ("a")) {
-		transform.Translate(Vector3.left / 7);
+		transform.Translate(Vector3.left / 15);
 	}
 		if (Input.GetKey ("d")) {
-		transform.Translate(Vector3.right / 7);
+		transform.Translate(Vector3.right / 15);
 	}
 }
